09ab9c5e2b5306e6e75b48e07e0a0a2c93a480f1
Yosniel Cabrera <yosniel.ch@gmail.com>
2024-11-08 15:24:23 -0300

    SendMessage controller modified to create a simple example

--- app/Http/Controllers/SendMessage.php
+++ app/Http/Controllers/SendMessage.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Events\MessageSent;
 
 class SendMessage extends Controller
 {
-    //
+    public function sendMessage(Request $request)
+    {
+        $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $message = $request->input('message');
+
+        broadcast(new MessageSent($message))->toOthers();
+
+        return response()->json(['status' => 'Message sent!']);
+    }
 }
