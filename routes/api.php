b286cd1e789c1b07fe241f4f53875df01a18b2c6
Yosniel Cabrera <ycabrera@tolber.io>
2025-06-17 17:12:32 -0300

    create a basic EP

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/users', function () {
+    return response()->json([
+        ['id' => 1, 'name' => 'Alice'],
+        ['id' => 2, 'name' => 'Bob']
+    ]);
 });
